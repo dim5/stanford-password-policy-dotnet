d8c5fee70ae6c030be68f98a91bd3ca7131efeeb
Daniel Marczin <dim5@users.noreply.github.com>
2019-06-04 22:05:08 +0200

    added curly braces

--- StanfordPasswordPolicy/StanfordPasswordValidator.cs
+++ StanfordPasswordPolicy/StanfordPasswordValidator.cs
@@ -20,32 +20,40 @@ namespace StanfordPasswordPolicy
             var errors = new List<IdentityError>();
 
             if (password.Length < 8)
+            {
                 errors.Add(new IdentityError
                 {
                     Code = ErrorCode.ShortLength,
                     Description = "Passwords must be at least 8 characters long."
                 });
+            }
 
             if (password.Length < 12 && !CheckSymbol(password))
+            {
                 errors.Add(new IdentityError
                 {
                     Code = ErrorCode.NoSymbol,
                     Description = "Passwords shorter than 12 characters must contain symbols."
                 });
+            }
 
             if (password.Length < 16 && !CheckNumber(password))
+            {
                 errors.Add(new IdentityError
                 {
                     Code = ErrorCode.NoNumber,
                     Description = "Passwords shorter than 16 characters must contain numbers."
                 });
+            }
 
             if (password.Length < 20 && !CheckMixedCase(password))
+            {
                 errors.Add(new IdentityError
                 {
                     Code = ErrorCode.NoMixedCase,
                     Description = "Passwords shorter than 20 characters must contain lower and upper case characters."
                 });
+            }
 
             var res = errors.Any() ? IdentityResult.Failed(errors.ToArray()) : IdentityResult.Success;
             return Task.FromResult(res);
